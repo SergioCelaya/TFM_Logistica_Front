c729038955ad80d2792dbc18cb3b26882a00253c
albegosu <albegosu@gmail.com>
2023-11-30 01:40:54 +0100

    Servicio conectado, visualización almacenes y actualización directa de detalle de almacen

--- src/app/components/detalle/detalle-almacen/detalle-almacen.component.ts
+++ src/app/components/detalle/detalle-almacen/detalle-almacen.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+// detalle-almacen.component.ts
+import { Component, OnInit } from '@angular/core';
+import { AlmacenService } from 'src/app/services/almacen.service';
+import { Almacen } from 'src/app/models/almacen.interface';
 
 @Component({
   selector: 'app-detalle-almacen',
   templateUrl: './detalle-almacen.component.html',
   styleUrls: ['./detalle-almacen.component.css']
 })
-export class DetalleAlmacenComponent {
+export class DetalleAlmacenComponent implements OnInit {
+  almacenSeleccionado: Almacen | null = null;
 
+  constructor(private almacenService: AlmacenService) {}
+
+  ngOnInit() {
+    // Obtén el primer almacén al inicializar el componente
+    this.almacenService.almacenSeleccionado$.subscribe((almacen) => {
+      this.almacenSeleccionado = almacen || null;
+    });
+
+    // Si el almacén seleccionado aún no se ha establecido (puede ser null),
+    // obtén el primer almacén para mostrar la información por defecto
+    if (!this.almacenSeleccionado) {
+      this.almacenService.getAll().then((almacenes) => {
+        if (almacenes && almacenes.length > 0) {
+          this.almacenService.actualizarAlmacenSeleccionado(almacenes[0]);
+        }
+      });
+    }
+  }
 }
--- src/app/services/almacen.service.ts
+++ src/app/services/almacen.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+// almacen.service.ts
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { firstValueFrom, BehaviorSubject, Observable } from 'rxjs';
+import { Almacen } from '../models/almacen.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlmacenService {
+  private baseUrl: string = 'http://localhost:3000/api/almacenes';
+  private httpClient = inject(HttpClient);
 
-  constructor() { }
+  private almacenSeleccionadoSubject: BehaviorSubject<Almacen | null> = new BehaviorSubject<Almacen | null>(null);
+  almacenSeleccionado$: Observable<Almacen | null> = this.almacenSeleccionadoSubject.asObservable();
+
+  constructor() {}
+
+  getAll() {
+    return firstValueFrom(
+      this.httpClient.get<Almacen[]>(this.baseUrl)
+    );
+  }
+
+  // Nueva función para actualizar el almacén seleccionado
+  actualizarAlmacenSeleccionado(almacen: Almacen | null) {
+    this.almacenSeleccionadoSubject.next(almacen);
+  }
 }
