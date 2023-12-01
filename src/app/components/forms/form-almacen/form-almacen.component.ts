0d10e9a587cbd86e9e02cf157d870bf295d776d9
Sergio <sercelaya@gmail.com>
2023-12-01 20:51:24 +0100

    Se ha incluido el servicio de imagenes he incluido un ejemplo en el formulario de almacenes

--- src/app/components/forms/form-almacen/form-almacen.component.ts
+++ src/app/components/forms/form-almacen/form-almacen.component.ts
@@ -2,52 +2,56 @@ import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlmacenService } from 'src/app/services/almacen.service';
+import { ImagenesService } from 'src/app/services/imagenes.service';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form-almacen',
   templateUrl: './form-almacen.component.html',
-  styleUrls: ['./form-almacen.component.css']
+  styleUrls: ['./form-almacen.component.css'],
 })
 export class FormAlmacenComponent {
-
   almacenForm: FormGroup;
   almacenService = inject(AlmacenService);
+  imagenesService = inject(ImagenesService);
   activatedRoute = inject(ActivatedRoute);
   router = inject(Router);
+  uploadedImage: any;
 
-  constructor(){
+  constructor() {
     this.almacenForm = new FormGroup({
       // idalmacen: new FormControl('', []),
       nombre_almacen: new FormControl('', [
         Validators.required,
         Validators.minLength(4),
-        Validators.maxLength(40)
+        Validators.maxLength(40),
       ]),
       long: new FormControl('', [
         Validators.required,
         Validators.pattern(/^(-?\d+(\.\d+)?)$/), // Longitud entre -180 y 180
         Validators.min(-180),
-        Validators.max(180)
+        Validators.max(180),
       ]),
       lat: new FormControl('', [
         Validators.required,
         Validators.pattern(/^(-?\d+(\.\d+)?)$/), // Latitud entre -90 y 90
         Validators.min(-90),
-        Validators.max(90)
+        Validators.max(90),
       ]),
       activo: new FormControl('', [Validators.required]),
-      imagen_almacen: new FormControl('', [Validators.required])
+      imagen_almacen: new FormControl('', [Validators.required]),
     });
   }
 
   // LÓGICA DEL FORMULARIO EXISTENTE O NUEVO
   ngOnInit(): void {
-
     this.activatedRoute.params.subscribe(async (params: any) => {
+      //SE NECESITA PARA GUARDAR LA IMAGEN Y TRAERLA
       let idalmacen: number = Number(params.idalmacen);
 
       if (idalmacen) {
+        //GUARDO EL ID DE ALMACEN PARA GUARDAR LA IMAGEN POSTERIORENTE
+        this.idAlmacen = idalmacen;
         //PINTAR ALMACEN EXISTENTE
         let response = await this.almacenService.getById(idalmacen);
 
@@ -56,100 +60,136 @@ export class FormAlmacenComponent {
           nombre_almacen: new FormControl(response.nombre_almacen, [
             Validators.required,
             Validators.minLength(4),
-            Validators.maxLength(40)
+            Validators.maxLength(40),
           ]),
           long: new FormControl(response.long, [
             Validators.required,
             Validators.pattern(/^(-?\d+(\.\d+)?)$/), // Longitud entre -180 y 180
             Validators.min(-180),
-            Validators.max(180)
+            Validators.max(180),
           ]),
           lat: new FormControl(response.lat, [
             Validators.required,
             Validators.pattern(/^(-?\d+(\.\d+)?)$/), // Latitud entre -90 y 90
             Validators.min(-90),
-            Validators.max(90)
+            Validators.max(90),
           ]),
           activo: new FormControl(response.activo, [Validators.required]),
           imagen_almacen: new FormControl(response.imagen_almacen, [
             Validators.required,
-            Validators.max(100)
-          ])
-      })
+            Validators.max(100),
+          ]),
+        });
       }
-  });
-}
+    });
+  }
 
-async submitForm(): Promise<void> {
-  if (this.almacenForm.value.idalmacen) {
-    try {
-      await Swal.fire({
-        title: '¿Quiere guardar los cambios?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#FFC007',
-        confirmButtonText: 'Guardar'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          // ACTUALIZACIÓN ALMACEN
-          let response = this.almacenService.updateAlmacen(this.almacenForm.value);
-          Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: 'Almacén actualizado correctamente',
-            showConfirmButton: false,
-            timer: 1500
-          });
-        }
-      })
-      this.router.navigate(['/almacenes']);
-      console.log(this.almacenForm.value);
-    } catch(error) {
-      Swal.fire({
-        position: 'center',
-        icon: 'error',
-        title: 'Error al actualizar el almacen',
-        showConfirmButton: false,
-        timer: 1500
-      })
-    }
-  } else {
-    try {
-      await Swal.fire({
-        title: '¿Quiere crear el almacén?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#FFC007',
-        confirmButtonText: 'Crear'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          // CREACIÓN NUEVO ALMACEN
-          let response = this.almacenService.create(this.almacenForm.value);
-          console.log(this.almacenForm.value);
-          Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: 'Almacén creado correctamente',
-            showConfirmButton: false,
-            timer: 1500
-          });
-        }
-      })
-      this.router.navigate(['/almacenes']);
-    } catch(error) {
-      Swal.fire({
-        position: 'center',
-        icon: 'error',
-        title: 'Ha habido un error, inténtelo de nuevo',
-        showConfirmButton: false,
-        timer: 1500
-      })
+  async submitForm(): Promise<void> {
+    if (this.almacenForm.value.idalmacen) {
+      try {
+        await Swal.fire({
+          title: '¿Quiere guardar los cambios?',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#FFC007',
+          confirmButtonText: 'Guardar',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // ACTUALIZACIÓN ALMACEN
+            let response = this.almacenService.updateAlmacen(
+              this.almacenForm.value
+            );
+            let resultImagen = this.guardarImagenAlmacen();
+            Swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: 'Almacén actualizado correctamente',
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
+        this.router.navigate(['/almacenes']);
+        console.log(this.almacenForm.value);
+      } catch (error) {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'Error al actualizar el almacen',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    } else {
+      try {
+        await Swal.fire({
+          title: '¿Quiere crear el almacén?',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#FFC007',
+          confirmButtonText: 'Crear',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // CREACIÓN NUEVO ALMACEN
+            let response = this.almacenService.create(this.almacenForm.value);
+            console.log(this.almacenForm.value);
+            Swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: 'Almacén creado correctamente',
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
+        this.router.navigate(['/almacenes']);
+      } catch (error) {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'Ha habido un error, inténtelo de nuevo',
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
     }
   }
-}
 
-checkControl(formcontrolName: string, validator: string): boolean | undefined {
-  return this.almacenForm.get(formcontrolName)?.hasError(validator) && this.almacenForm.get(formcontrolName)?.touched;
-}
+  checkControl(
+    formcontrolName: string,
+    validator: string
+  ): boolean | undefined {
+    return (
+      this.almacenForm.get(formcontrolName)?.hasError(validator) &&
+      this.almacenForm.get(formcontrolName)?.touched
+    );
+  }
+  // TODO LO RELACIONADO CON LA IMAGEN, PRIMERO SE MUESTRA TRAS ELEGIRLA
+  // AL PRESIONAR EN EL BOTÓN ACEPTAR SE ENVIA AL BACK
 
+  url: any;
+  imagenFile: File | undefined;
+  idAlmacen:number | undefined;
+
+  onChange(event: any) {
+    var reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.url = event.target.result;
+    };
+
+    reader.onerror = (event: any) => {
+      //TODO
+      console.log('No se puede leer el fichero: ' + event.target.error.code);
+    };
+    reader.readAsDataURL(event.target.files[0]);
+    this.imagenFile = event.target.files[0];
+  }
+
+  guardarImagenAlmacen() {
+    if (this.imagenFile && this.idAlmacen) {
+      this.imagenesService.guardarImagenAlmacen(this.imagenFile, this.idAlmacen);
+    }else{
+      //TODO: Lanzar error no se ha seleccionado una imagen
+    }
+  }
 }
---
+++ src/app/services/imagenes.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ImagenesService {
+  private httpClient = inject(HttpClient);
+  private baseUrl: string = 'http://localhost:3000/api/imagenes';
+  private urlAlmacenes: string = '/getImagenAlmacen/';
+  private urlEmpleados: string = '/getImagenEmpleado/';
+  private uploadAlmacen: string = '/uploadImagenAlmacen/';
+  private uploadEmpleado: string = '/uploadImagenEmpleado/:idEmpleado';
+
+  getImagenAlmacen(nombreImg: string): string {
+    return this.baseUrl + this.urlAlmacenes + nombreImg;
+  }
+
+  getImagenEmpleado(nombreImg: string): string {
+    return this.baseUrl + this.urlEmpleados + nombreImg;
+  }
+
+  guardarImagenEmpleado(imagen: File, idEmpleado: number) {
+    try {
+      if (imagen) {
+        const data = new FormData();
+        data.append('image', imagen, imagen.name);
+        return this.httpClient.post(this.baseUrl + this.uploadEmpleado + idEmpleado, data);
+      } else {
+        return null;
+      }
+    } catch (Error) {
+      return Error;
+    }
+  }
+
+  async guardarImagenAlmacen(imagen: File, idAlmacen: number) {
+    try {
+      if (imagen) {
+        const data = new FormData();
+        data.append('imagen', imagen, imagen.name);
+        let req = await firstValueFrom(this.httpClient.post(this.baseUrl + this.uploadAlmacen + idAlmacen, data));
+        console.log(req);
+        return req;
+      } else {
+        return null;
+      }
+    } catch (Error) {
+      //TODO
+      console.log("Errrror al mandar la imagen")
+      return Error;
+    }
+  }
+
+  constructor() {}
+}
