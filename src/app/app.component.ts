3083211e4ab5dc3f61a44ea76e08f11fa64fc083
albegosu <albegosu@gmail.com>
2023-12-06 10:17:53 +0100

    Menu oculto en login, menu filtrado según puesto usando getUser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TFM_Logistica_Front';
+    constructor(private router: Router) {}
 
+    isLoginPage(): boolean {
+        return this.router.url === '/login';
+    }
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -8,18 +8,36 @@ import { MapService } from 'src/app/services/map.service';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit{
+export class MenuComponent implements OnInit {
   servicioMapa = inject(MapService);
   isLoggedIn$!: Observable<boolean>;
+  userPuesto: string = '';
 
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
     this.isLoggedIn$ = this.authService.isLoggedIn;
+    this.authService.getUser().then((empleado) => {
+      this.userPuesto = empleado.puesto;
+      console.log(this.userPuesto);
+    })
   }
 
-  onLogout(){
+  onLogout() {
     this.authService.logout();
   }
 
+  // Helper methods to check user's position
+  isEmpleado(): boolean {
+    return this.userPuesto === 'Empleado';
+  }
+
+  isEncargado(): boolean {
+    return this.userPuesto === 'Encargado';
+  }
+
+  isAdmin(): boolean {
+    return this.userPuesto === 'Administrador';
+  }
 }
+
