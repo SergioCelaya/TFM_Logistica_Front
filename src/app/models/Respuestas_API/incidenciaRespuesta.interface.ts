e52edec782511a65029c8f2013c04ff96ed49c29
Sonia-GE <sonia.fmayoral@gmail.com>
2023-12-11 15:31:52 +0100

    Preparacion para mostrar numero de pedido en detalle incidencia

--- src/app/models/Respuestas_API/incidenciaRespuesta.interface.ts
+++ src/app/models/Respuestas_API/incidenciaRespuesta.interface.ts
@@ -5,6 +5,7 @@ idincidencia: number;
 titulo: string;
 descripcion: string;
 idpedido_asociado: number;
+numero_pedido: string;
 tipo_incidencia: string;
 vista: boolean;
 usuario_asignado:EmpleadoRespuesta;
