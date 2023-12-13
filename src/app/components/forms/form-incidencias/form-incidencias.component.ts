65b9db8ceb2f6cb936c1a6217f12ac7ab7a5426f
Sergio <sercelaya@gmail.com>
2023-12-13 23:08:16 +0100

    corrección

--- src/app/components/forms/form-incidencias/form-incidencias.component.ts
+++ src/app/components/forms/form-incidencias/form-incidencias.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { IncidenciasService } from 'src/app/services/incidencias.service';
 import Swal from 'sweetalert2';
 import { allIncidencia } from 'src/app/models/Respuestas_API/allIncidencias.interface';
+import { PedidosService } from 'src/app/services/pedidos.service';
+import { Incidencia } from 'src/app/models/incidencia.interface';
 
 @Component({
   selector: 'app-form-incidencias',
@@ -14,10 +16,11 @@ export class FormIncidenciasComponent {
   incidenciasForm: FormGroup;
   IncidenciasService = inject(IncidenciasService);
   activatedRoute = inject(ActivatedRoute);
+  PedidoService = inject(PedidosService);
   router = inject(Router);
   idIncidencia: number | undefined;
   Resultado: allIncidencia | null = null;
-  modo:string = "create";
+  modo: string = 'create';
 
   constructor() {
     this.incidenciasForm = new FormGroup({
@@ -37,9 +40,8 @@ export class FormIncidenciasComponent {
       tipo_incidencia: new FormControl('', [Validators.required]),
 
       idIncidencia: new FormControl('', []),
-
-      idpedido_asociado: new FormControl('', [Validators.required]),
-
+      idpedido_asociado: new FormControl('', []),
+      numeroPedidoAsociado: new FormControl('', [Validators.required]),
       vista: new FormControl('', [Validators.required]),
     });
   }
@@ -49,13 +51,15 @@ export class FormIncidenciasComponent {
     this.activatedRoute.params.subscribe(async (params: any) => {
       //SE NECESITA PARA GUARDAR LA IMAGEN Y TRAERLA
       let idincidencia: number = Number(params.idincidencia);
-      
+
       if (idincidencia) {
         this.modo = 'update';
         //GUARDO EL ID DE INCIDENCIA PARA GUARDAR LA IMAGEN POSTERIORENTE
         this.idIncidencia = idincidencia;
         //PINTAR INCIDENCIA EXISTENTE
-        let response = await this.IncidenciasService.getByIdConNumPedido(idincidencia);
+        let response = await this.IncidenciasService.getByIdConNumPedido(
+          idincidencia
+        );
         this.incidenciasForm = new FormGroup({
           idincidencia: new FormControl(response.idincidencia, []),
           titulo: new FormControl(response.titulo, [
@@ -75,8 +79,8 @@ export class FormIncidenciasComponent {
           ]),
 
           idIncidencia: new FormControl(response.idincidencia, []),
-
-          idpedido_asociado: new FormControl(response.numero_pedido, [
+          idpedido_asociado: new FormControl(response.numero_pedido, []),
+          numeroPedidoAsociado: new FormControl(response.numero_pedido, [
             Validators.required,
           ]),
 
@@ -106,7 +110,7 @@ export class FormIncidenciasComponent {
           cancelButtonColor: '#dc3545',
           cancelButtonText: 'Cancelar',
           confirmButtonColor: '#157347',
-          confirmButtonText: 'Guardar'
+          confirmButtonText: 'Guardar',
         }).then((result) => {
           if (result.isConfirmed) {
             // ACTUALIZACIÓN INCIDENCIA
@@ -152,13 +156,21 @@ export class FormIncidenciasComponent {
           cancelButtonText: 'Cancelar',
           confirmButtonColor: '#157347',
           confirmButtonText: 'Crear',
-        }).then((result) => {
+        }).then(async (result) => {
           if (result.isConfirmed) {
             // CREACIÓN NUEVO INCIDENCIA
-
-            let response = this.IncidenciasService.create(
-              this.incidenciasForm.value
+            let pedidoAsociado = await this.PedidoService.getPedidoByNumPedido(
+              this.incidenciasForm.value.numeroPedidoAsociado
             );
+            const incidencia: Incidencia = {
+              descripcion: this.incidenciasForm.value.descripcion,
+              idpedido_asociado: pedidoAsociado.idPedido,
+              tipo_incidencia: '1',
+              titulo: this.incidenciasForm.value.titulo,
+              vista: false,
+              idincidencia: '',
+            };
+            let response = this.IncidenciasService.create(incidencia);
 
             Swal.fire({
               position: 'center',
